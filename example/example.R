e960abe9e68ed79362d4d6dcf71a67c496782751
Han Zhang <zhangh.ustc@gmail.com>
2024-11-21 01:05:12 -0500

    add Controller class

--- example/example.R
+++ example/example.R
@@ -3,22 +3,29 @@ library(R6)
 library(rlang)
 library(survival)
 
+
+## max number of enrolled patients
+n_patients <- 1000
+
 source('./example/DefinePlaceboArm.R')
 source('./example/DefineLowDoseArm.R')
 source('./example/DefineHighDoseArm.R')
-source('./example/DefineTrial.R')
 source('./example/DefineFutilityEvent.R')
 source('./example/DefinePfsInterimEvent.R')
 source('./example/DefinePfsFinalEvent.R')
 source('./example/DefineOsFinalEvent.R')
 
-trial_listener <- Listener$new()
-trial_listener$add_events(
+source('./example/DefineTrial.R')
+
+listener <- Listener$new()
+listener$add_events(
     futility_event,
     pfs_interim_event,
     pfs_final_event,
     os_final_event
   )
 
-trial_listener$monitor(trial)
+controller <- Controller$new(trial, listener)
+controller$run()
+
 
