79b468e68212693bdb2103825f86d9117b83e9da
Han Zhang <zhangh.ustc@gmail.com>
2025-03-08 12:54:06 -0500

    add function to fit logistic regression; support fit models for placebo vs each of multiple treated arms.

--- R/fitLogistic.R
+++ R/fitLogistic.R
@@ -1,32 +1,39 @@
-#' Fit Cox proportional hazard ratio model
+#' Fit generalized logistic regression model
 #'
 #' @description
-#' Fit Cox proportional hazards model on an time-to-event endpoint.
+#' Fit generalized logistic regression model on an binary endpoint.
 #'
 #' @param endpoint Character. Name of the endpoint in \code{data}.
 #' @param placebo Character. String indicating the placebo in \code{data$arm}.
 #' @param data Data frame. Usually it is a locked data set.
+#' @param alternative a character string specifying the alternative hypothesis,
+#' must be one of \code{"greater"} or \code{"less"}. No default value.
+#' \code{"greater"} means superiority of treatment over placebo is established
+#' by an odds ratio greater than 1 because a
+#' logistic regression model is fitted with \code{endpoint ~ I(arm != placebo)}.
 #' @param ... Subset conditions compatible with \code{dplyr::filter}.
-#' \code{coxph} will be fitted on this subset only. This argument can be useful
+#' \code{glm} will be fitted on this subset only. This argument can be useful
 #' to create a subset of data for analysis when a trial consists of more
 #' than two arms. By default, it is not specified,
 #' all data will be used to fit the model. More than one condition can be
 #' specified in \code{...}, e.g.,
-#' \code{fitCoxph('pfs', 'pbo', data, arm \%in\% c('pbo', 'low dose'), pfs > 0.5)},
+#' \code{fitLogistic('cfb', 'pbo', data, arm \%in\% c('pbo', 'low dose'), cfb > 0.5)},
 #' which is equivalent to:
-#' \code{fitCoxph('pfs', 'pbo', data, arm \%in\% c('pbo', 'low dose') & pfs > 0.5)}.
+#' \code{fitLogistic('cfb', 'pbo', data, arm \%in\% c('pbo', 'low dose') & cfb > 0.5)}.
+#' Note that if more than one treatment arm are present in the data after
+#' applying filter in \code{...}, models are fitted for placebo verse
+#' each of the treatment arms.
 #'
 #' @returns a data frame with three columns:
 #' \describe{
-#' \item{\code{p}}{one-sided p-value for
-#' log hazard ratio (alternative hypothesis: log hazard ratio > 0). }
+#' \item{\code{p}}{one-sided p-value for log odds ratio (placebo vs treated). }
 #' \item{\code{info}}{the number of events of the endpoint in the subset. }
-#' \item{\code{z}}{the z statistics of log hazard ratios. }
+#' \item{\code{z}}{the z statistics of log odds ratio. }
 #' }
 #'
 #' @export
 #'
-fitCoxph <- function(endpoint, placebo, data, ...) {
+fitLogistic <- function(endpoint, placebo, data, alternative, ...) {
 
   if(!is.character(endpoint) || length(endpoint) != 1){
     stop("endpoint must be a single character string")
@@ -40,8 +47,9 @@ fitCoxph <- function(endpoint, placebo, data, ...) {
     stop("data must be a data frame")
   }
 
-  event <- paste0(endpoint, '_event')
-  required_cols <- c('arm', endpoint, event)
+  alternative <- match.arg(alternative, choices = c('greater', 'less'))
+
+  required_cols <- c('arm', endpoint)
   if(!all(required_cols %in% names(data))){
     stop('Columns <',
          paste0(setdiff(required_cols, names(data)), collapse = ', '),
@@ -57,7 +65,7 @@ fitCoxph <- function(endpoint, placebo, data, ...) {
       data %>% dplyr::filter(...)
     },
     error = function(e){
-      stop('Error in filtering data for coxph test. ',
+      stop('Error in filtering data for fitting a logistic regression model. ',
            'Please check condition in ..., ',
            'which should be compatible with dplyr::filter. ')
     })
@@ -71,20 +79,23 @@ fitCoxph <- function(endpoint, placebo, data, ...) {
   treatment_arms <- setdiff(unique(filtered_data$arm), placebo)
 
   # Create the formula
-  formula_str <- paste0("Surv(time = ", endpoint,
-                        ", event = ", endpoint, "_event) ~ ",
-                        "I(arm == '", placebo, "')")
+  formula_str <- paste0(endpoint, " ~ I(arm != '", placebo, "')")
 
   ret <- NULL
+
   for(trt_arm in treatment_arms){
     sub_data <- filtered_data %>% dplyr::filter(arm %in% c(placebo, trt_arm))
 
-    # Fit the Cox model
-    fit <- summary(coxph(as.formula(formula_str), data = sub_data))$coef
+    # Fit the logistic regression model
+    fit <- summary(glm(as.formula(formula_str),
+                       family = binomial,
+                       data = sub_data)
+                   )$coef
 
-    z <- fit[1, 'coef']/fit[1, 'se(coef)']
-    p <- 1 - pnorm(z)
-    info <- sum(sub_data[[event]] %in% 1)
+    z <- fit[2, 'z value']
+    p <- ifelse(alternative == 'greater', 1 - pnorm(z), pnorm(z))
+
+    info <- sum(!is.na(sub_data[[endpoint]]))
 
     ret <- rbind(ret, data.frame(arm = trt_arm, placebo = placebo,
                                  p = p, info = info, z = z
@@ -92,7 +103,7 @@ fitCoxph <- function(endpoint, placebo, data, ...) {
                  )
   }
 
-  class(ret) <- c('fit_coxph', class(ret))
+  class(ret) <- c('fit_logistic', class(ret))
   ret
 }
 
