43f0f38f8d94be25278e595db796a62fdf404b34
Han Zhang <zhangh.ustc@gmail.com>
2025-03-08 04:07:03 -0500

    use unicode only to pass cran check

--- R/createSticker.R
+++ R/createSticker.R
@@ -2,25 +2,27 @@
 
 createSticker <- function(){
 
-  font_add_google('Caveat')
+#   font_add_google('Caveat')
+#
+#   sticker(
+#     subplot = image_read('~/Downloads/iStock-466742904.png'),
+#     package = 'TrialSimulator',
+#     s_width = 2.5,
+#     s_height = 2.5,
+#     s_x = 1,
+#     s_y = 1.1,
+#     p_y = .4,
+#     p_size = 16,
+#     #p_color = '#00AFFF',
+#     p_family = 'Caveat',
+#     white_around_sticker = TRUE,
+#     h_size = 0,
+#     h_color = "#FFFFFF",
+#     filename = 'man/figures/logo.png',
+#     dpi = 300
+#   )
 
-  sticker(
-    subplot = image_read('~/Downloads/iStock-466742904.png'),
-    package = 'TrialSimulator',
-    s_width = 2.5,
-    s_height = 2.5,
-    s_x = 1,
-    s_y = 1.1,
-    p_y = .4,
-    p_size = 16,
-    #p_color = '#00AFFF',
-    p_family = 'Caveat',
-    white_around_sticker = TRUE,
-    h_size = 0,
-    h_color = "#FFFFFF",
-    filename = 'man/figures/logo.png',
-    dpi = 300
-  )
+  invisible(NULL)
 
 }
 
