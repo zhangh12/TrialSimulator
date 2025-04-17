5862ad6c1697b5478baf03384a887a102a557eec
Han Zhang <zhangh.ustc@gmail.com>
2025-04-17 12:36:40 -0400

    add wrapper functions to replace class constructors. Vignettes are updated accordingly.

---
+++ R/wrapperOfController.R
@@ -0,0 +1,21 @@
+#' Define a Controller
+#'
+#' @description
+#' Define a controller of a trial. This is a user-friendly wrapper for
+#' the class constructor \code{Controller$new}. Users who are not familiar with
+#' the concept of classes may consider using this wrapper directly.
+#'
+#' @param trial a \code{Trial} object.
+#' @param listener a \code{Listener} object.
+#'
+#' @examples
+#'
+#' ## controller <- controller(trial, listener)
+#'
+#' @export
+#'
+controller = function(trial, listener){
+
+  Controller$new(trial = trial, listener = listener)
+
+}
