1961801fa87dc722ad90455108da60392b168ef8
Han Zhang <zhangh.ustc@gmail.com>
2025-04-27 03:49:37 -0400

    add more unit tests

--- R/Listener.R
+++ R/Listener.R
@@ -5,12 +5,14 @@
 #' the class constructor \code{Listener$new}. Users who are not familiar with
 #' the concept of classes may consider using this wrapper directly.
 #'
+#' @param silent logical. \code{TRUE} to mute messages.
+#'
 #' @examples
 #'
 #' listener <- listener()
 #'
 #' @export
 #'
-listener <- function(){
-  Listeners$new()
+listener <- function(silent = FALSE){
+  Listeners$new(silent = silent)
 }
