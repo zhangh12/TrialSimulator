41a8b87cf8f3ea5c860407428ef840a387a2206c
Han Zhang <zhangh.ustc@gmail.com>
2025-09-16 22:05:12 -0400

    support minimum treatment duration in milestone determination; update docs

--- R/enrollment.R
+++ R/enrollment.R
@@ -13,11 +13,20 @@
 #' \code{TrialSimulator}.
 #'
 #' @param n integer. Number of randomized patients.
+#' @param ... subset conditions compatible with \code{dplyr::filter}. Number
+#' of randomized patients will be counted on subset of trial data only.
 #' @param arms vector of character. Name of arms on which the number of patients
 #' is counted. If \code{NULL}, use all arms that are not yet removed from the
 #' trial by the time of calculation.
-#' @param ... subset conditions compatible with \code{dplyr::filter}. Number
-#' of randomized patients will be counted on subset of trial data only.
+#' @param min_treatment_duration numeric. Zero or positive value.
+#' minimum treatment duration of enrolled patients.
+#' Default is 0, i.e., looking for triggering time based on number of enrolled
+#' patients in population specified by \code{...} and \code{arms}. If positive,
+#' it means that milestone is triggered when a specific number of enrolled
+#' patients have received treatment for at least \code{min_treatment_duration}
+#' duration. It is users' responsibility to assure that the unit of
+#' \code{min_treatment_duration} are consistent with
+#' readout of non-tte endpoints, dropout time, and trial duration.
 #'
 #' @returns an object of class `Condition`
 #'
@@ -32,10 +41,15 @@
 #' ## ensure sufficient sample size in high dose + placebo
 #' enrollment(n = 1000, arms = c('high dose', 'placebo'))
 #'
+#' ## ensure sufficient treatment duration
+#' enrollment(n = 500, min_treatment_duration = 2)
+#'
 #'
 #' @export
-enrollment <- function(n, ..., arms = NULL){
+enrollment <- function(n, ..., arms = NULL, min_treatment_duration = 0){
 
-  EnrollmentCountCondition$new(n, ..., arms = arms)
+  EnrollmentCountCondition$new(n, ...,
+                               arms = arms,
+                               min_treatment_duration = min_treatment_duration)
 
 }
